1bb5cf65b41e106f779cadaebd0179908a4e0e4e
Cyprien Pannier <cyprien.pannier@gmail.com>
2021-07-05 14:52:03 +0200

    remove unnecessary code

--- development/src/me/cyppan/core.clj
+++ development/src/me/cyppan/core.clj
@@ -1,6 +1,1 @@
-(ns me.cyppan.core
-  (:require [me.cyppan.schema.interface :as schema]
-            [schema.core :as s]))
-
-(println (schema/test))
-(def prismatic-schema s/Str)
+(ns me.cyppan.core)
